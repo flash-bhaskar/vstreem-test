727b6e7f27d6ecbad4a474bec3d8a5b1cf132b02
flashbhaskar <bhaskar@arraigntech.com>
2021-04-22 15:29:20 +0530

    first comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-vstreem-test';
+
+  constructor(private http:HttpClient){
+}
+ 
+  test(){
+    console.log("start");
+    this.http.post
+    ("http://3.129.148.62/account/update-password",{
+      password:"Bhaskar360",
+      newpassword:"Bhaskar063"
+    },{
+      headers:new HttpHeaders().set("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiYWNjZXNzIiwicmVnaXN0YXJ0aW9uIl0sInVzZXJfbmFtZSI6ImJoYXNrYXIiLCJzY29wZSI6WyJSRUFEIiwiV1JJVEUiXSwiZXhwIjoxNjE4OTY1MzA0LCJhdXRob3JpdGllcyI6WyJ1cGRhdGVfcHJvZmlsZSIsInJlYWRfcHJvZmlsZSJdLCJqdGkiOiIxYzA4MDc0Ny1mZjVlLTQwNjktYjhmMi00MmZmNThhYWM0MWYiLCJlbWFpbCI6ImJoYXNrYXJhczE5OTlAZ21haWwuY29tIiwiY2xpZW50X2lkIjoibW9iaWxlIiwicmVzb3VyY2VpZHMiOlsiYWNjZXNzIiwicmVnaXN0YXJ0aW9uIl19.wkPMGJhG5xSL3hdXWf0l5uqZ_XHWtaHFEjycZL6UNNE")
+    }
+    ).subscribe(responseData=>console.log(responseData),
+    error =>console.log(error)
+  ); 
+  }
+
+
 }
